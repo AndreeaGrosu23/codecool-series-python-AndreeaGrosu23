9b972ad06e317e67c96b3b3678f7e5f5fa8ded3b
Andreea Grosu <andreea.grosu87@gmail.com>
2020-03-25 18:48:47 +0200

    Add pagination

--- data/queries.py
+++ data/queries.py
@@ -1,7 +1,7 @@
 from data import data_manager
 
 
-def get_shows(limit):
+def get_shows(data):
     return data_manager.execute_select(
         "SELECT title, year, runtime, genre, ROUND(rating, 2) AS rating, trailer, homepage "
         "FROM shows "
@@ -14,6 +14,28 @@ def get_shows(limit):
         "ON sh_genres.genre_id = genres.id "
         "GROUP BY 1) AS gs "
         "ON shows.id = gs.show_id "
-        "ORDER BY rating DESC LIMIT %(limit)s;",
-        {'limit':limit}
+        "ORDER BY rating DESC "
+        "OFFSET (%s-1)*15 "
+        "LIMIT %s;",
+        (data['page_id'],
+         data['limit'])
+    )
+
+def get_shows_per_page(page):
+    return data_manager.execute_select(
+        "SELECT title, year, runtime, genre, ROUND(rating, 2) AS rating, trailer, homepage "
+        "FROM shows "
+        "INNER JOIN (SELECT show_id, string_agg(DISTINCT genres.name, ',') AS genre "
+        "FROM genres "
+        "INNER JOIN (SELECT * FROM ("
+        "SELECT show_id, genre_id, row_number() OVER ("
+        "PARTITION BY show_id ORDER BY genre_id) AS rownum FROM show_genres) tmp "
+        "WHERE rownum <= 3 ) AS sh_genres "
+        "ON sh_genres.genre_id = genres.id "
+        "GROUP BY 1) AS gs "
+        "ON shows.id = gs.show_id "
+        "ORDER BY rating DESC "
+        "OFFSET (%(page)s-1)*15"
+        "LIMIT 15;",
+        {"page": page}
     )
--- main.py
+++ main.py
@@ -6,8 +6,29 @@ app = Flask('codecool_series')
 
 @app.route('/')
 def index():
-    shows = queries.get_shows(15)
-    return render_template('index.html', shows=shows)
+    page_id = 1
+    data = {
+        'limit': 15,
+        'page_id': 1
+    }
+    shows = queries.get_shows(data)
+    return render_template('index.html', shows=shows, page_id=page_id)
+
+
+@app.route('/get-shows')
+def get_shows():
+    queries.get_shows()
+
+
+@app.route('/get-shows/<page_id>')
+def get_shows_per_page(page_id):
+    try:
+        shows = queries.get_shows_per_page(page_id)
+        return render_template('index.html', shows=shows, page_id=int(page_id))
+    except:
+        page_id = 0
+        shows = queries.get_shows_per_page(1)
+        return render_template('index.html', shows=shows, page_id=page_id)
 
 
 @app.route('/design')
