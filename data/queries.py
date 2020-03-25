af694b121aaedaa92f9644216ef39c65c3a782dc
Andreea Grosu <andreea.grosu87@gmail.com>
2020-03-25 20:57:50 +0200

    Add pagination and sorted view

--- data/queries.py
+++ data/queries.py
@@ -15,13 +15,82 @@ def get_shows(data):
         "GROUP BY 1) AS gs "
         "ON shows.id = gs.show_id "
         "ORDER BY rating DESC "
-        "OFFSET (%s-1)*15 "
-        "LIMIT %s;",
-        (data['page_id'],
-         data['limit'])
+        "OFFSET (%(page_id)s-1)*%(limit)s "
+        "LIMIT %(limit)s;",
+        # {'header': data['header'],
+        #  'order': data['order'],
+        {'page_id': data['page_id'],
+         'limit': data['limit']}
     )
 
-def get_shows_per_page(page):
+def sorted_by_title_ASC(data):
+    return data_manager.execute_select(
+        "SELECT title, year, runtime, genre, ROUND(rating, 2) AS rating, trailer, homepage "
+        "FROM shows "
+        "INNER JOIN (SELECT show_id, string_agg(DISTINCT genres.name, ',') AS genre "
+        "FROM genres "
+        "INNER JOIN (SELECT * FROM ("
+        "SELECT show_id, genre_id, row_number() OVER ("
+        "PARTITION BY show_id ORDER BY genre_id) AS rownum FROM show_genres) tmp "
+        "WHERE rownum <= 3 ) AS sh_genres "
+        "ON sh_genres.genre_id = genres.id "
+        "GROUP BY 1) AS gs "
+        "ON shows.id = gs.show_id "
+        "ORDER BY title ASC "
+        "OFFSET (%(page_id)s-1)*%(limit)s "
+        "LIMIT %(limit)s;",
+        # {'header': data['header'],
+        #  'order': data['order'],
+        {'page_id': data['page_id'],
+         'limit': data['limit']}
+    )
+
+
+def sorted_by_title_DESC(data):
+    return data_manager.execute_select(
+        "SELECT title, year, runtime, genre, ROUND(rating, 2) AS rating, trailer, homepage "
+        "FROM shows "
+        "INNER JOIN (SELECT show_id, string_agg(DISTINCT genres.name, ',') AS genre "
+        "FROM genres "
+        "INNER JOIN (SELECT * FROM ("
+        "SELECT show_id, genre_id, row_number() OVER ("
+        "PARTITION BY show_id ORDER BY genre_id) AS rownum FROM show_genres) tmp "
+        "WHERE rownum <= 3 ) AS sh_genres "
+        "ON sh_genres.genre_id = genres.id "
+        "GROUP BY 1) AS gs "
+        "ON shows.id = gs.show_id "
+        "ORDER BY title DESC "
+        "OFFSET (%(page_id)s-1)*%(limit)s "
+        "LIMIT %(limit)s;",
+        # {'header': data['header'],
+        #  'order': data['order'],
+        {'page_id': data['page_id'],
+         'limit': data['limit']}
+    )
+
+def sorted_by_rating_ASC(data):
+    return data_manager.execute_select(
+        "SELECT title, year, runtime, genre, ROUND(rating, 2) AS rating, trailer, homepage "
+        "FROM shows "
+        "INNER JOIN (SELECT show_id, string_agg(DISTINCT genres.name, ',') AS genre "
+        "FROM genres "
+        "INNER JOIN (SELECT * FROM ("
+        "SELECT show_id, genre_id, row_number() OVER ("
+        "PARTITION BY show_id ORDER BY genre_id) AS rownum FROM show_genres) tmp "
+        "WHERE rownum <= 3 ) AS sh_genres "
+        "ON sh_genres.genre_id = genres.id "
+        "GROUP BY 1) AS gs "
+        "ON shows.id = gs.show_id "
+        "ORDER BY rating ASC "
+        "OFFSET (%(page_id)s-1)*%(limit)s "
+        "LIMIT %(limit)s;",
+        # {'header': data['header'],
+        #  'order': data['order'],
+        {'page_id': data['page_id'],
+         'limit': data['limit']}
+    )
+
+def sorted_by_rating_DESC(data):
     return data_manager.execute_select(
         "SELECT title, year, runtime, genre, ROUND(rating, 2) AS rating, trailer, homepage "
         "FROM shows "
@@ -35,7 +104,10 @@ def get_shows_per_page(page):
         "GROUP BY 1) AS gs "
         "ON shows.id = gs.show_id "
         "ORDER BY rating DESC "
-        "OFFSET (%(page)s-1)*15"
-        "LIMIT 15;",
-        {"page": page}
+        "OFFSET (%(page_id)s-1)*%(limit)s "
+        "LIMIT %(limit)s;",
+        # {'header': data['header'],
+        #  'order': data['order'],
+        {'page_id': data['page_id'],
+         'limit': data['limit']}
     )
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from data import queries
 
 app = Flask('codecool_series')
@@ -12,6 +12,16 @@ def index():
         'page_id': 1
     }
     shows = queries.get_shows(data)
+    header = request.args.get(key = 'header')
+    order = request.args.get(key = 'order')
+    if header == "title" and order == "asc" :
+        shows = queries.sorted_by_title_ASC(data)
+    elif header == "title" and order == "desc" :
+        shows = queries.sorted_by_title_DESC(data)
+    elif header == "rating" and order == "asc" :
+        shows = queries.sorted_by_rating_ASC(data)
+    elif header == "rating" and order == "desc" :
+        shows = queries.sorted_by_rating_DESC(data)
     return render_template('index.html', shows=shows, page_id=page_id)
 
 
@@ -22,13 +32,21 @@ def get_shows():
 
 @app.route('/get-shows/<page_id>')
 def get_shows_per_page(page_id):
+    data = {
+        'limit': 15,
+        'page_id': page_id
+    }
     try:
-        shows = queries.get_shows_per_page(page_id)
+        shows = queries.get_shows(data)
         return render_template('index.html', shows=shows, page_id=int(page_id))
     except:
         page_id = 0
-        shows = queries.get_shows_per_page(1)
-        return render_template('index.html', shows=shows, page_id=page_id)
+        data = {
+            'limit': 15,
+            'page_id': 1
+        }
+        shows = queries.get_shows(data)
+        return render_template('index.html', shows=shows, page_id=int(page_id))
 
 
 @app.route('/design')
